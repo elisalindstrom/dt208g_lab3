e9f68ed2e725f6f889fbb5c58d98a67e702cae84
elisalindstrom <elisalindstroem@gmail.com>
2026-04-25 11:51:37 +0200

    Lagt till funktion och styling för konvertering av värden + not found sida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
-
 // Importera komponenter
 import { Home } from './home/home';
 import { Convert } from './convert/convert';
 import { About } from './about/about';
+import { NotFound } from './not-found/not-found';
 
 export const routes: Routes = [
     { path: 'home', component: Home },
     { path: 'convert', component: Convert },
     { path: 'about', component: About },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', component: NotFound}
 ];
--- src/app/convert/convert.ts
+++ src/app/convert/convert.ts
@@ -1,9 +1,44 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.html',
   styleUrl: './convert.scss',
 })
-export class Convert {}
+
+export class Convert {
+  value: number = 0;
+  result: number = 0;
+  unit: string = '';
+
+  // Metoder
+  meterToFeet(): void {
+    if (this.value === 0) return;
+
+    this.result = this.value * 3.2808399;
+    this.unit = 'fot';
+  }
+
+  feetToMeter(): void {
+    if (this.value === 0) return;
+
+    this.result = this.value / 3.2808399;
+    this.unit = 'meter';
+  }
+
+  celsiusToFahrenheit(): void {
+    if (this.value === 0) return;
+
+    this.result = (this.value * 9.0 / 5.0) + 32.0;
+    this.unit = 'fahrenheit';
+  }
+
+  fahrenheitToCelsius(): void {
+    if (this.value === 0) return;
+
+    this.result = (this.value - 32.0) * 5.0 / 9.0;
+    this.unit = 'celsius';
+  }
+}
